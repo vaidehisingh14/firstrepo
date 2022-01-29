68e56c9de21e04a6ff8bdf349ba3e1ee7b461e02
vaidehisingh14 <98647149+vaidehisingh14@users.noreply.github.com>
2022-01-29 18:09:36 +0530

    Add files via upload
    Practice codes

---
+++ ASCIIVal.java
@@ -0,0 +1,14 @@
+package session1;
+
+public class ASCIIVal {
+
+	public static void main(String [] args) {
+
+		int c = 'a';
+		int b = 'A';
+
+		System.out.println(c);
+		System.out.println(b);
+		
+	}
+}
